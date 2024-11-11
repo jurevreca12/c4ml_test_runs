062cba873a6e13ece38e2c10a9bcf682d0aa8744
jvreca <jure.vreca@ijs.si>
2024-11-11 14:50:37 +0000

    Added convolutional test.

---
+++ conv_model.py
@@ -0,0 +1,60 @@
+import brevitas
+import brevitas.nn as qnn
+import numpy as np
+import torch
+from torch.nn import Module
+from qonnx.core.datatype import DataType
+from qonnx.util.basic import gen_finn_dt_tensor
+from quantizers import CommonWeightQuant
+from quantizers import IntBiasQuant
+from quantizers import IntActQuant
+
+def get_conv_layer_model(
+    input_size, input_ch, output_ch, kernel_size, iq, wq, bq, oq, depthwise=False
+):
+    class ConvLayerModel(Module):
+        def __init__(self):
+            super(ConvLayerModel, self).__init__()
+            self.ishape = (1, input_ch) + input_size
+            self.conv = qnn.QuantConv2d(
+                in_channels=input_ch,
+                out_channels=output_ch,
+                groups=output_ch if depthwise else 1,
+                kernel_size=kernel_size,
+                stride=1,
+                bias=True,
+                weight_quant=CommonWeightQuant,
+                weight_bit_width=wq,
+                weight_scaling_impl_type="const",
+                weight_scaling_init=1 if wq == 1 else 2 ** (wq - 1) - 1,
+                bias_quant=IntBiasQuant,
+                bias_bit_width=bq,
+                bias_scaling_impl_type="const",
+                bias_scaling_init=2 ** (bq - 1) - 1,
+                input_quant=IntActQuant,
+                input_bit_width=iq,
+                input_scaling_impl_type="const",
+                input_scaling_init=1 if iq == 1 else 2 ** (iq - 1),
+                output_quant=IntActQuant,
+                output_bit_width=oq,
+                output_scaling_impl_type="const",
+                output_scaling_init=1 if oq == 1 else 2 ** (oq - 1) - 1,
+            )
+
+        def forward(self, x):
+            return self.conv(x)
+
+    model = ConvLayerModel()
+    wshape = (output_ch, 1 if depthwise else input_ch, kernel_size[0], kernel_size[1])
+    bshape = (output_ch,)
+    # set seed for repeatability
+    np.random.seed(42)
+    wq_type = DataType[f"INT{wq}"] if wq > 1 else DataType["BIPOLAR"]
+    iq_type = DataType[f"INT{iq}"] if iq > 1 else DataType["BIPOLAR"]
+    weights = gen_finn_dt_tensor(wq_type, wshape)
+    bias = gen_finn_dt_tensor(DataType[f"INT{bq}"], bshape)
+    model.conv.weight = torch.nn.Parameter(torch.from_numpy(weights).float())
+    model.conv.bias = torch.nn.Parameter(torch.from_numpy(bias).float())
+    ishape = (32,) + model.ishape[1:]
+    input_data = gen_finn_dt_tensor(iq_type, ishape)
+    return model, input_data
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import chisel4ml
 from chisel4ml import transform
 from chisel4ml import generate
 from linear_model import get_linear_layer_model
+from conv_model import get_conv_layer_model
 from server import get_server, create_server
 from test_model import test_model
 import argparse
@@ -26,7 +27,7 @@ linear_layer_var_in_features_exp = {
     "oq": (4,)
 }
 
-linear_layer_var_iq_features_exp = {
+linear_layer_var_iq_exp = {
     "in_features": (32,),
     "out_features": (32,),
     "bias":  (True,),
@@ -36,7 +37,7 @@ linear_layer_var_iq_features_exp = {
     "oq": (4,)
 }
 
-linear_layer_var_wq_features_exp = {
+linear_layer_var_wq_exp = {
     "in_features": (32,),
     "out_features": (32,),
     "bias":  (True,),
@@ -46,16 +47,45 @@ linear_layer_var_wq_features_exp = {
     "oq": (4,)
 }
 
+conv_layer_var_input_ch_exp = {
+    "input_size": ((16, 16),),
+    "input_ch": (1, 2, 4, 8, 16),
+    "output_ch": (1,),
+    "kernel_size": ((3,3),),
+    "iq": (4,),
+    "wq": (4,),
+    "bq": (8,),
+    "oq": (4,)
+}
+
+maxpool_layer_var_input_size = {
+    "input_size": ((4,4), (8,8), (12,12), (16,16)),
+    "kernel_size": ((3, 3),),
+    "iq": (4,)
+}
+
 experiments = (
-    (linear_layer_var_in_features_exp, get_linear_layer_model, "linear_layer_var_in_features"),
-    (linear_layer_var_iq_features_exp, get_linear_layer_model, "linear_layer_var_iq_features"),
-    (linear_layer_var_wq_features_exp, get_linear_layer_model, "linear_layer_var_wq_features"),
+    #(linear_layer_var_in_features_exp, get_linear_layer_model, "linear_layer_var_in_features"),
+    #(linear_layer_var_iq_exp, get_linear_layer_model, "linear_layer_var_iq"),
+    #(linear_layer_var_wq_exp, get_linear_layer_model, "linear_layer_var_wq"),
+    (conv_layer_var_input_ch_exp, get_conv_layer_model, "conv_layer_var_input_ch"),
+    #(maxpool_layer_var_input_size, get_maxpool_layer_model, "maxpool_layer_var_input_size"),
 )
 current_exp = 0
 
 
 def get_work_dir(keys, values, base):
-    str_list = list(map(lambda kv: f"{kv[0]}{str(kv[1])}", zip(keys, values)))
+    def to_string(kv):
+        key, vals = kv
+        if isinstance(vals, (tuple, list)):
+            val_str = ""
+            for val in vals:
+                val_str += str(val) + "_"
+            val_str = val_str[:-1] 
+            return f"{key}{val_str}"
+        else:
+            return f"{key}{vals}"
+    str_list = list(map(lambda kv: to_string(kv), zip(keys, values)))
     specific_dir = functools.reduce(lambda a,b: a + "_" + b, str_list)
     return SCRIPT_DIR + base + specific_dir
 
