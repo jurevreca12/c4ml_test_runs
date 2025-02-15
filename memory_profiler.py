31d3fb69e71345652b3e862a70774f2ddff9fbd2
Jure Vreca (jure.vreca@ijs.si <jvreca@jastreb.ijs.si>
2025-02-15 21:32:12 +0100

    memory proflier working for finn.

--- memory_profiler.py
+++ memory_profiler.py
@@ -3,107 +3,46 @@ import subprocess
 import time
 import sys
 import json
+import threading
+
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+    return wrapper
 
 
 class ProcessContainer:
-    def __init__(self, pid=None):
-        self.command = command
-        self.p = subp
+    def __init__(self, pid):
         self.pid = pid
         self.max_vms_memory = 0
         self.max_rss_memory = 0
-        if self.p is None:
-            self.execution_state = False
-        else:
-            self.execution_state = True
+        self.active = True
 
-    def execute(self):
-        self.max_vms_memory = 0
-        self.max_rss_memory = 0
-        if self.p is None:
-            self.p = subprocess.Popen(self.command, shell=False)
-            self.execution_state = True
-            self.pid = self.p.pid
+    def stop(self):
+        self.active = False
 
     def poll(self):
-        if not self.check_execution_state():
-            return False
+        pp = psutil.Process(self.pid)
 
-        try:
-            pp = psutil.Process(self.pid)
+        # obtain a list of the subprocess and all its descendants
+        descendants = list(pp.children(recursive=True))
+        descendants = descendants + [pp]
 
-            # obtain a list of the subprocess and all its descendants
-            descendants = list(pp.children(recursive=True))
-            descendants = descendants + [pp]
+        rss_memory = 0
+        vms_memory = 0
+        # calculate and sum up the memory of the subprocess
+        # and all its descendants
+        for descendant in descendants:
+            mem_info = descendant.memory_info()
+            rss_memory += mem_info[0]
+            vms_memory += mem_info[1]
+        self.max_vms_memory = max(self.max_vms_memory, vms_memory)
+        self.max_rss_memory = max(self.max_rss_memory, rss_memory)
 
-            rss_memory = 0
-            vms_memory = 0
-
-            # calculate and sum up the memory of the subprocess
-            # and all its descendants
-            for descendant in descendants:
-                try:
-                    mem_info = descendant.memory_info()
-
-                    rss_memory += mem_info[0]
-                    vms_memory += mem_info[1]
-                except psutil.NoSuchProcess:
-                    # sometimes a subprocess descendant will have terminated
-                    # between the time we obtain a list of descendants, and the
-                    # time we actually poll this descendant's memory usage.
-                    pass
-            self.max_vms_memory = max(self.max_vms_memory, vms_memory)
-            self.max_rss_memory = max(self.max_rss_memory, rss_memory)
-
-        except psutil.NoSuchProcess:
-            return self.check_execution_state()
-
-        return self.check_execution_state()
-
-    def is_running(self):
-        return psutil.pid_exists(self.pid)
-
-    def check_execution_state(self):
-        if not self.execution_state:
-            return False
-        if self.is_running():
-            return True
-        self.executation_state = False
-        return False
-
-    def close(self, kill=False):
-        try:
-            pp = psutil.Process(self.pid)
-            if kill:
-                pp.kill()
-            else:
-                pp.terminate()
-        except psutil.NoSuchProcess:
-            pass
-
-
-if __name__ == "__main__":
-    proc_cont = ProcessContainer(sys.argv[1:])
-    try:
-        proc_cont.execute()
-        # poll as often as possible; otherwise the subprocess might
-        # "sneak" in some extra memory usage while you aren't looking
-        while proc_cont.poll():
-            time.sleep(0.5)
-    finally:
-        # make sure that we don't leave the process dangling?
-        proc_cont.close()
-
-    print("return code:", proc_cont.p.returncode)
-    print(f"max_vms_memory: {proc_cont.max_vms_memory / (1024 * 1024)} MiB")
-    print(f"max_rss_memory: {proc_cont.max_rss_memory / (1024 * 1024)} MiB")
-    print(f"max_vms_memory: {proc_cont.max_vms_memory}")
-    print(f"max_rss_memory: {proc_cont.max_rss_memory}")
-    result_dict = {
-        "sys_args": sys.argv,
-        "max_vms_memory": proc_cont.max_vms_memory,
-        "max_rss_memory": proc_cont.max_rss_memory,
-    }
-    with open("memory_info.json", "w") as mem_file:
-        json.dump(result_dict, mem_file)
-    sys.exit(0)
+    @threaded
+    def profile(self):
+        while self.active:
+            self.poll()
+            time.sleep(1.0) 
--- test_finn.py
+++ test_finn.py
@@ -5,19 +5,12 @@ import time
 import shutil
 import json
 from memory_profiler import ProcessContainer
-from threading import Thread
-
-
-def mem_profiling_thread_fn(proc_cont):
-    while proc_cont.poll():
-        time.sleep(0.5)
 
 def test_finn(qonnx_model_file, work_dir, base_dir):
     curr_dir = os.getcwd()
     pid = os.getpid()
     mem_prof = ProcessContainer(pid=pid)
-    mem_prof_thread = Thread(target=mem_profiling_thread_fn, args=(mem_prof,))
-    mem_prof_thread.start()
+    thread_handle = mem_prof.profile()
     if not os.path.exists(work_dir):
         os.makedirs(work_dir)
     os.chdir(work_dir)
@@ -41,11 +34,12 @@ def test_finn(qonnx_model_file, work_dir, base_dir):
     )
     build.build_dataflow_cfg(qonnx_model_file, cfg_stitched_ip)
     duration = time.perf_counter() - starttime
-    mem_prof_thread.join()
+    mem_prof.stop()
+    thread_handle.join()
     info_dict = {}
     info_dict['total_duration'] = duration
-    info_dict['max_vms_memory'] = mem_prof.max_vms_memory
-    info_dict['max_rss_memory'] = mem_prof.max_rss_memory
+    info_dict['total_max_vms_memory'] = mem_prof.max_vms_memory
+    info_dict['total_max_rss_memory'] = mem_prof.max_rss_memory
     info_dict['tool'] = "finn"
     with open(f"{work_dir}/info.json", 'w') as info_file:
         json.dump(info_dict, info_file)
