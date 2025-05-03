f74de4872cb2d75b3d1ce047873f0291c964a173
jvreca <jure.vreca@ijs.si>
2025-05-03 13:02:24 +0000

    Fixed sed command

--- build_prj.tcl
+++ build_prj.tcl
@@ -202,7 +202,11 @@ if {$opt(cosim)} {
     remove_recursive_log_wave
     set old_pwd [pwd]
     cd ${project_name}_prj/solution1/sim/verilog/
+<<<<<<< Updated upstream
     exec -- sed -i "s/xsim --noieeewarnings/xsim --maxdeltaid 99999999 --noieeewarnings/g" run_xsim.sh
+=======
+    exec -- sed -i "s/xsim --noieeewarnings/xsim --maxdeltaid 99999999 --noieeewarnings/g" run_xsim.sh 
+>>>>>>> Stashed changes
     source run_sim.tcl
     cd $old_pwd
 
